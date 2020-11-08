3ac891ab3dd28a53ec15fed3b3bd208ae1b10cb0
Aubre Martin <73319163+Mar-Aubre@users.noreply.github.com>
2020-11-08 15:09:59 -0700

    Update
    Commented a Line out of the program, propably not nessary

--- .gitattributes2.py
+++ .gitattributes2.py
@@ -4,7 +4,7 @@
 def reprogject (layer):
     master_crs = 'EPSG:26914'
     if layer.crs != master_crs:
-        print(list_of_lables[idx], 'is not on the master projection, reprojecting now..')
+        #print(list_of_lables[idx], 'is not on the master projection, reprojecting now..')
         layer = layer.to_crs(master_crs) 
         print (layer.crs, 'and', master_crs)
     

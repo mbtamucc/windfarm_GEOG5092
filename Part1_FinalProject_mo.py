81152f97f93d3ab109bee40914dc56315a4c44f7
mbtamucc <72160492+mbtamucc@users.noreply.github.com>
2020-11-30 16:49:55 -0700

    Add files via upload

---
+++ Part1_FinalProject_mo.py
@@ -0,0 +1,157 @@
+import numpy as np
+import os
+import rasterio 
+from rasterio.plot import show, show_hist
+import glob
+import scipy
+#from lab5functions import *
+import pandas as pd
+import geopandas as gpd
+#import netCDF4 as nc
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from osgeo import gdal, gdal_array, osr, ogr
+import rasterio 
+import pandas as pd 
+import geopandas as gpd
+import numpy as np
+import rioxarray as rxr
+import os
+from shapely.geometry import Point, LineString, Polygon, box
+from rasterio.mask import mask
+from rasterio.crs import CRS
+from rasterio.warp import calculate_default_transform, reproject, Resampling
+from fiona.crs import from_epsg
+
+in_data_dir = r'.\data'
+
+#this file contains 4 functions:
+#reproject vector layers
+#reproject raster layers 
+#parse features from gdb for improved rasterio readability
+#convert shapefiles to rasters
+
+def reprogject (layer):
+    "This is the function that reproject the vector layers"
+    master_crs = 'EPSG:26914'
+    if layer.crs != master_crs:
+        #print(list_of_lables[idx], 'is not on the master projection, reprojecting now..')
+        layer = layer.to_crs(master_crs) 
+        print (layer.crs, 'and', master_crs)
+    
+    return layer
+
+def reproject_r(in_path, out_path):
+    'This is the function to reproject the raster data '
+    dst_crs = 'EPSG:26914'
+    with rasterio.open(in_path) as src:
+        transform, width, height = calculate_default_transform(
+            src.crs, dst_crs, src.width, src.height, *src.bounds)
+        kwargs = src.meta.copy()
+        kwargs.update({
+            'crs': dst_crs,
+            'transform': transform,
+            'width': width,
+            'height': height
+        })
+        with rasterio.open(out_path, 'w', **kwargs) as dst:
+            for i in range(1, src.count + 1):
+                reproject(
+                    source=rasterio.band(src, i),
+                    destination=rasterio.band(dst, i),
+                    src_transform=src.transform,
+                    src_crs=src.crs,
+                    dst_transform=transform,
+                    dst_crs=dst_crs,
+                    resampling=Resampling.nearest)
+    return(out_path)
+
+def getFeatures(gdf):
+    """Function to parse features from GeoDataFrame in such a manner that rasterio wants them"""
+    import json
+    return [json.loads(gdf.to_json())['features'][0]['geometry']]
+
+
+#convert shapefile to raster (work in progress)
+def main(shapefile):
+    #making the shapefile as an object.
+    input_shp = ogr.Open(shapefile)
+    shp_layer = input_shp.GetLayer()
+    pixel_size = 0.1
+    xmin, xmax, ymin, ymax = shp_layer.GetExtent()
+    ds = gdal.Rasterize(output_raster, shapefile, xRes=pixel_size, yRes=pixel_size, 
+                        burnValues=255, outputBounds=[xmin, ymin, xmax, ymax], 
+                        outputType=gdal.GDT_Byte)
+    ds = None
+
+
+
+#reading fields in gdal and ogr with hopes to rasterize (work in progress)
+shipping = (os.path.join(in_data_dir, 'shippinglanes/shippinglanes.shp'))
+input_shp = ogr.Open(shipping)
+#getting layer information of shapefile.
+shp_layer = input_shp.GetLayer()
+
+dataSource = ogr.Open(shipping)
+daLayer = dataSource.GetLayer(0)
+layerDefinition = daLayer.GetLayerDefn()
+for i in range(layerDefinition.GetFieldCount()):
+    print(layerDefinition.GetFieldDefn(i).GetName())
+
+
+#rasterizing test using rasterio and geopandas
+import rasterio
+from rasterio.features import rasterize
+from rasterio.transform import from_bounds
+import geopandas as gpd
+# Load some sample data
+df = gpd.read_file(os.path.join(in_data_dir, 'shippinglanes/shippinglanes.shp')
+# lose a bit of resolution, but this is a fairly large file, and this is only an example.
+shape = 1000, 1000
+transform = rasterio.transform.from_bounds(*df['geometry'].total_bounds, *shape)
+rasterize_rivernet = rasterize(
+    [(shape, 1) for shape in df['geometry']],
+    out_shape=shape,
+    transform=transform,
+    fill=0,
+    all_touched=True,
+    dtype=rasterio.uint8)
+
+with rasterio.open(
+    'rasterized-results.tif', 'w',
+    driver='GTiff',
+    dtype=rasterio.uint8,
+    count=1,
+    width=shape[0],
+    height=shape[1],
+    transform=transform
+) as dst:
+    dst.write(rasterize_rivernet, indexes=1)
+
+#rasterize 3 work in progress
+shp_fn = (gpd.read_file(os.path.join(in_data_dir, 'shippinglanes/shippinglanes.shp')))
+rst_fn = (in_data_dir + './reproject_corpuschristi_dem.tif')
+out_fn = (in_data_dir + './rasterized.tif')
+
+rst = rasterio.open(rst_fn)
+
+meta = rst.meta.copy()
+meta.update(compress='lzw')
+
+with rasterio.open(out_fn, 'w+', **meta) as out:
+    out_arr = out.read(1)
+    out.write_band(1, out_arr)
+
+
+#masking function (work in progress)
+import rasterio
+from rasterio.plot import show
+from rasterio.plot import show_hist
+from rasterio.mask import mask
+from shapely.geometry import box
+import geopandas as gpd
+from fiona.crs import from_epsg
+import pycrs
+
+
--- windfarmproject.py
+++ windfarmproject.py
@@ -1,3 +1,33 @@
+<<<<<<< HEAD
+import rasterio
+import pandas as pd
+import geopandas as gpd
+import scipy
+import glob
+from netCDF4 import Dataset
+import os
+
+
+os.getcwd()
+
+with rasterio.open(in_data_dir + './corpuschristi_dem.tif', 'r') as dem_file:
+    dem = dem_file.read(1)
+    dem_profile = dem_file.profile
+    #calculates the slope and aspect using the DEM with a cell size of 30
+    #as part of the analysis, we could determine the slope and aspect of each suitable parcel
+    s,a = slopeAspect(dem, 30)
+    #recalculates the aspect to 8 cardinal directions using the fuction 
+    aspect = reclassAspect(a)
+    #using a bin size of 10, recalculate the slope grid into 10 classes using the function
+    s = reclassByHisto(s, 10)
+    #row first, last row. first column, last column
+    show(dem[1100:8350,1100:8350])
+    dem_profile
+    dem.shape
+    print(s,a)
+#set bounds to dem extents
+#create a polygon frame and use geopandas clip
+=======
 import rasterio
 import pandas as pd
 import geopandas as gpd
@@ -25,3 +55,4 @@ with rasterio.open(in_data_dir + './corpuschristi_dem.tif', 'r') as dem_file:
     print(s,a)
 #set bounds to dem extents
 #create a polygon frame and use geopandas clip
+>>>>>>> 395c9cd0030662d4347ee091f555bfcab351699b

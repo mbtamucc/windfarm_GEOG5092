dc151ea4c28663b8d89b5e7aa74c62a94a9cadb6
Aubre Martin <73319163+Mar-Aubre@users.noreply.github.com>
2020-12-01 17:59:02 -0700

    Add files via upload
    Here is my cleaned version of the code

--- Part1_FinalProject.py
+++ Part1_FinalProject.py
@@ -53,94 +53,18 @@ def reproject_r(in_path, out_path):
                     resampling=Resampling.nearest)
     return(out_path)
 
-practice = reproject_r('./corpuschristi_dem.tif', './reproject_corpuschristi_dem.tif')
-print(practice)
+reproject_dem = reproject_r('.\\corpuschristi_dem.tif', '.\\reproject_corpuschristi_dem.tif')
 
-with rasterio.open(in_data_dir + '.\\wtk_conus_100m_mean_masked.tif', 'r') as dem_file:
-    wind = dem_file.read(1) 
-    #wind = reprogject(dem)
-    #practice = reproject_raster(wind)
-    #print(practic.crs)
-    #crs_UTM13 = CRS.from_string('EPSG:26914')
-    # Reproject the data using the crs object
-    #layer_UTM13 = wind.rxr.reproject(crs_wgs84)
-    #layer_UTM13.rxr.crs
-#    suit_wind_speed = np.where( 7.0 < wind, 1, 0)
-#    print('The number of sites where wind speed is greater than 7 m ', suit_wind_speed.sum())
+with rasterio.open(in_data_dir + '.\\reproject_corpuschristi_dem.tif', 'r') as dem_file: 
+    beach = dem_file.read(1) 
     
-#with rasterio.open(in_data_dir + '.\\corpuschristi_dem.tif', 'r') as dem_file:
-#    beach = dem_file.read(1) 
-    #beach = reprogject(dem)
-#    suit_beach = np.where( beach == 0.0 , 1, 0)
-#    print('The number of sites where elevation is at sea level ', suit_beach.sum())
-
-data_dir = "L5_data"
-
-# Input raster
-fp = os.path.join(data_dir, "p188r018_7t20020529_z34__LV-FIN.tif")
-
-# Output raster
-out_tif = os.path.join(data_dir, "Helsinki_Masked.tif")
-
-# Read the data
-data = rasterio.open(fp)
-
-# Visualize the NIR band
-show((data, 4), cmap='terrain')
+    suit_beach = np.where( beach == 0.0 , 1, 0)
+    print('The number of sites where elevation is at sea level ', suit_beach.sum())
 
 
-# WGS84 coordinates
-minx, miny = 24.60, 60.00
-maxx, maxy = 25.22, 60.35
-bbox = box(minx, miny, maxx, maxy)
+reproject_wind = reproject_r('.\\wtk_conus_100m_mean_masked.tif', '.\\reprojectDocuments_wtk_conus_100m_mean_masked.tif') 
 
-geo = gpd.GeoDataFrame({'geometry': bbox}, index=[0], crs=from_epsg(4326))
-print(geo)
-
-# Project the Polygon into same CRS as the grid
-geo = geo.to_crs(crs=data.crs.data)
-
-# Print crs
-geo.crs
-
-def getFeatures(gdf):
-    """Function to parse features from GeoDataFrame in such a manner that rasterio wants them"""
-    import json
-    return [json.loads(gdf.to_json())['features'][0]['geometry']]
-
-
-coords = getFeatures(geo)
-print(coords)
-
-
-# Clip the raster with Polygon
-out_img, out_transform = mask(dataset=data, shapes=coords, crop=True)
-
-
-# Copy the metadata
-out_meta = data.meta.copy()
-print(out_meta)
-
-
-
-# Parse EPSG code
-epsg_code = int(data.crs.data['init'][5:])
-print(epsg_code)
-
-out_meta.update({"driver": "GTiff",
-                 "height": out_img.shape[1],
-                 "width": out_img.shape[2],
-                 "transform": out_transform,
-                 "crs": pycrs.parser.from_epsg_code(epsg_code).to_proj4()}
-                         )
-
-
-with rasterio.open(out_tif, "w", **out_meta) as dest:
-        dest.write(out_img)
-        
-
-# Open the clipped raster file
-clipped = rasterio.open(out_tif)
-
-# Visualize
-show((clipped, 5), cmap='terrain')
+with rasterio.open(in_data_dir + '.\\reprojectDocuments_wtk_conus_100m_mean_masked.tif', 'r') as dem_file:
+    wind = dem_file.read(1) 
+    suit_wind_speed = np.where( 7.0 < wind, 1, 0)
+    print('The number of sites where wind speed is greater than 7 m ', suit_wind_speed.sum())
